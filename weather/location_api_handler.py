3805f1c1bcce838c92c30a93f22e834f4965ac43
dolevifr <dolevif@gmail.com>
2023-07-23 23:20:52 +0300

    SUBMIT VERSION
    added documention, updated css files

--- weather/location_api_handler.py
+++ weather/location_api_handler.py
@@ -6,16 +6,16 @@ from datetime import datetime
 class WeatherAPIWrapper:
     def get_location_weather_data(self, location):
         response = requests.get(
-            f'https://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q={location}&days=2&aqi=no&alerts=no')
-        return response.json()
+            f'https://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q={location}&days=7&aqi=no&alerts=no')
+        return response
 
     def get_day_of_week_from_epoch(self,location):
-        response_json = self.get_location_weather_data(location)
+        response_json = self.get_location_weather_data(location).json()
         location_data_from_json = response_json['location']
         epoch_timestamp = location_data_from_json['localtime_epoch']
         # Convert epoch time to a datetime object
         dt_object = datetime.fromtimestamp(epoch_timestamp)
-        # Get the day of the week (0: Monday, 1: Tuesday, ..., 6: Sunday)
+        # Get the day of the week, 0 is monday 6 is sunday
         day_of_week_number = dt_object.weekday()
         return day_of_week_number
 
--- weather/views.py
+++ weather/views.py
@@ -12,7 +12,7 @@ days_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 class IndexPageView(View):
     def get(self, request):
         API_wrapper = WeatherAPIWrapper()
-        primary_locations_data = [API_wrapper.get_location_weather_data(city) for city in cities]
+        primary_locations_data = [API_wrapper.get_location_weather_data(city).json() for city in cities]
         return render(request, 'index_page.html', {"locations_data": primary_locations_data})
 
 
@@ -20,8 +20,7 @@ class ForcastInfo(View):
     def get(self, request):
         API_wrapper = WeatherAPIWrapper()
         location = request.GET.get('location')
-        response = requests.get(
-            f'https://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q={location}&days=7&aqi=no&alerts=no')
+        response = API_wrapper.get_location_weather_data(location)
         if response.status_code == 200:
             data = response.json()
             curr_day_num = API_wrapper.get_day_of_week_from_epoch(location)
