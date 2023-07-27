ac6d4dec7c9c260d4d00eeaa7d2e43d5618f9566
dolevifr <dolevif@gmail.com>
2023-07-27 13:55:22 +0300

    ADDED DATES AND MULTIPLE CITIES
    added a feature that handles two cities with dates option. the ui currently need a little fix. also need to add support by the api

---
+++ weather/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from datetime import date, timedelta
+
+class InputForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Calculate the start and end dates for the date range (7 days from today)
+        today = date.today()
+        end_date = today + timedelta(days=6)
+        start_date = today
+        self.fields['location'] = forms.CharField(label="City Location", max_length=30)
+        # Set the DateField widget's attributes to limit the date range
+        self.fields['start_date'] = forms.DateField(
+            label="Start Date",
+            initial=start_date,
+            widget=forms.DateInput(attrs={'type': 'date', 'min': start_date, 'max': end_date})
+        )
+        self.fields['end_date'] = forms.DateField(
+            label="End Date",
+            initial=end_date,
+            widget=forms.DateInput(attrs={'type': 'date', 'min': start_date, 'max': end_date})
+        )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get('start_date')
+        end_date = cleaned_data.get('end_date')
+
+        if end_date < start_date:
+            raise forms.ValidationError("End date cannot be earlier than the start date.")
+
+        return cleaned_data
--- weather/views.py
+++ weather/views.py
@@ -3,9 +3,9 @@ from django.views import View
 import requests
 from .API_KEY import WEATHER_API_KEY
 from .location_api_handler import WeatherAPIWrapper
-from datetime import datetime
+from .forms import InputForm
 
-cities = ["New-York", "Tokyo","Tel Aviv","Dubai"]
+cities = ["New-York", "Tokyo", "Rishon-LeZiyyon", "Dubai"]
 days_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
 
@@ -13,7 +13,10 @@ class IndexPageView(View):
     def get(self, request):
         API_wrapper = WeatherAPIWrapper()
         primary_locations_data = [API_wrapper.get_location_weather_data(city).json() for city in cities]
-        return render(request, 'index_page.html', {"locations_data": primary_locations_data})
+
+        return render(request, 'index_page.html', {'locations_data': primary_locations_data, 'cities': cities,
+                                                   'weather_input_form': InputForm()})
+
 
 
 class ForcastInfo(View):
@@ -24,8 +27,9 @@ class ForcastInfo(View):
         if response.status_code == 200:
             data = response.json()
             curr_day_num = API_wrapper.get_day_of_week_from_epoch(location)
-            return render(request, 'forcast_info.html', {'data': data,'curr_day_num': curr_day_num})
-        else: #handles error in page not existing places
+            return render(request, 'forcast_info.html',
+                          {'data': data, 'curr_day_num': curr_day_num, 'days_of_week': days_of_week})
+        else:  # handles error in page not existing places
             return render(request, 'error_page.html')
 
 
